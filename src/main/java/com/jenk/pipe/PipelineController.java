e384783aa28eb0fda554eebc7f5827b3a0558ca3
tejait <teja.chintapalli246@gmail.com>
2026-03-05 16:02:25 +0530

    first commit of jenkins project

---
+++ src/main/java/com/jenk/pipe/PipelineController.java
@@ -0,0 +1,14 @@
+package com.jenk.pipe;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class PipelineController {
+    @GetMapping("maven")
+    public String mavenBuild(){
+        return "Teja APP Maven Build";
+    }
+}
